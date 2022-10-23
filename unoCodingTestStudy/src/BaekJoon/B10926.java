6a5252cbb2c1d6acf117127c2c0fc91ff0781288
uno-kim <zhfldk014745@naver.com>
2022-10-23 21:26:48 +0900

    백준 10926 문제 풀이

---
+++ unoCodingTestStudy/src/BaekJoon/B10926.java
@@ -0,0 +1,16 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B10926
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		System.out.println(input + "??!");
+		sc.close();
+	}
+}
